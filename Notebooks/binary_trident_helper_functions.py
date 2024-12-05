610ae7fa4519e292aa00eee5e4168cf934797595
Vikrant Jaltare <jvikrantt@gmail.com>
2024-12-05 12:09:46 -0800

    Committing recent changes

---
+++ Notebooks/binary_trident_helper_functions.py
@@ -0,0 +1,52 @@
+import jax
+import jax.numpy as jnp
+from jax import vmap, jit
+
+
+
+## define binary thresholding function: states [-1, 1]
+def binary_activation(x, threshold, noise_sd, key):
+    """
+    Binary activation function
+    """
+    # key, key2 = jax.random.split(key, 2)
+
+    # generate noise
+    noise = jax.random.normal(key, shape = x.shape) * noise_sd
+
+    # inject noise
+    x = x + noise
+
+    s = jnp.where(
+        x < threshold, 0.0, 1.0
+    )
+
+    return s
+
+## helper function
+@jax.jit
+def gaussian_cdf(x, mu, sigma):
+    return jax.scipy.stats.norm.cdf(x, loc = mu, scale = sigma)
+
+@jax.jit
+def gaussian_pdf(x, mu, sigma):
+    return jax.scipy.stats.norm.pdf(x, loc = mu, scale = sigma)
+
+@jax.jit
+def bin_expected_state(x, threshold, noise_sd):
+    e = gaussian_cdf(x = x - threshold, mu = 0, sigma = noise_sd)
+    return e
+
+@jax.custom_vjp
+def custom_binary_gradient(x, threshold, noise_sd, key):
+    return binary_activation(x = x, threshold = threshold, noise_sd = noise_sd, key = key)
+
+def custom_binary_gradient_fwd(x, threshold, noise_sd, key):
+    return custom_binary_gradient(x, threshold, noise_sd, key), (x, threshold, noise_sd)
+
+def custom_binary_gradient_bwd(residuals, gradients):
+    x, threshold, noise_sd = residuals
+    grad = gaussian_pdf(x = x - threshold, mu = 0, sigma = noise_sd)
+    return (grad*gradients, None, None, None)
+
+custom_binary_gradient.defvjp(custom_binary_gradient_fwd, custom_binary_gradient_bwd)
---
+++ Notebooks/epic_ai_architecture_v1.py
@@ -0,0 +1,128 @@
+"""
+Modules for the EPIC AI chip architecture.
+Learning is done using binary stochastic version of Trident algorithm
+"""
+
+import jax
+import jax.numpy as jnp
+import optax
+import flax
+from flax import linen as nn
+from functools import partial
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from collections import defaultdict
+from binary_trident_helper_functions import *
+
+
+
+## defining the core module
+class EICCore(nn.Module):
+    """
+    Initializes number of cores and operations that each core performs.
+
+    - Each core receives inputs x which are 256x1 vectors
+    - The core splits the inputs into two identical parts
+    - Each part is the shuffled randomly.
+    - Core then performs W(xp - xn)
+    - Returns output after applying the binary activation function.
+    """
+
+    ## define the number of rows ans columns
+    key: jax.random.PRNGKey
+    num_inputs: int
+    num_outputs: int
+
+    @staticmethod
+    def intercore_connectivity(num_rows, num_cols, alpha, key):
+        """
+        Defines intercore connectivity matrix.
+        Follows a simplified arrangement where each core is connected to a neighbor on the same row or column with some probability alpha.
+        """
+        V = jnp.zeros((num_rows, num_cols, num_rows, num_cols))
+        key, split_key = jax.random.split(key)
+
+        random_conn = jax.random.uniform(split_key, (num_cols, num_cols)) < alpha
+
+        V = V.at[jnp.arange(num_rows), :, jnp.arange(num_rows), :].set(random_conn)
+
+        key, split_key = jax.random.split(key)
+        random_conn = jax.random.uniform(split_key, (num_rows, num_rows)) < alpha
+
+        V = V.at[:, jnp.arange(num_cols), :, jnp.arange(num_cols)].set(random_conn)
+
+        return V
+    
+    @staticmethod
+    def split_and_shuffle(input):
+        """
+        Split input into two parts. Shuffle each part chunk-wise.
+
+        Args:
+        1. vector
+
+        Outputs:
+        1. xp and xn
+        """
+
+        ## get the chunk size
+        chunk_size = len(input) // 4
+
+        ## split the input
+        xp, xn = input.copy(), input.copy()
+
+        ## generate shuffle indices for each chunk
+        num_chunks = 4
+        key, subkey = jax.random.split(key)
+
+        def shuffle_chunk(chunk, shuffle_key):
+            idx = jax.random.permutation(shuffle_key, chunk_size)
+         
+
+
+
+
+    
+    @nn.compact
+    def __call__(self, x):
+        """
+        For every core simply perform matrix-vector product
+        """
+        uw = self.param(
+            "uw", 
+            nn.initializers.xavier_uniform(), (self.num_outputs, self.num_inputs)
+        )
+
+        # reparametrization to ensure non-negative weights
+        W = jnp.square(uw)
+
+        y = jnp.dot(W, x)
+
+        y = custom_binary_gradient(x=y, threshold=0.0, noise_sd=0.1, key=self.key)
+
+        return y
+    
+    def split_inputs(self, x):
+        """
+        Split inputs into two parts.
+        Shuffle each vector in chunks of 64
+        """
+
+
+    
+    def get_weights(self, params):
+        """
+        Returns weights for visualization
+        """
+        weights = params["params"]["uw"]
+        weights = jnp.square(weights)
+        return weights
+    
+
+## defining the chip architecture module
+
+    
+
+
+
+
