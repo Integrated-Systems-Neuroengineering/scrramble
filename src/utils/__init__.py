4a46bd500b35f0d53814d5c63d262785a1a334eb
vikrant <vjaltare@ucsd.edu>
2025-05-02 18:46:22 -0700

    modified __init__.py in utils to expose intercore_connectivity. Verify!

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,7 @@
 
 from .clipping_ste import clipping_ste
 from .rram_quantize import rram_quantize
+from .intercore_connectivity import intercore_connectivity, plot_connectivity_matrix
 
 
-
-__all__  = ["clipping_ste", "rram_quantize"]
+__all__  = ["clipping_ste", "rram_quantize", "intercore_connectivity", "plot_connectivity_matrix"]
