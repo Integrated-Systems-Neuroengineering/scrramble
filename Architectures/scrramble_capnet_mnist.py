77d44bf5778e4472862111b9d5802a53396193d8
vikrant <vjaltare@ucsd.edu>
2025-07-02 19:26:56 -0700

    in progress caps net

---
+++ Architectures/scrramble_capnet_mnist.py
@@ -0,0 +1,90 @@
+"""
+Use this script to train ScRRAMBLe CapsNet on MNIST. 
+
+Created on: 07/02/2025
+"""
+import jax
+import math
+import jax.numpy as jnp
+import optax
+import flax
+from flax import nnx
+from flax.nnx.nn import initializers
+from typing import Callable
+import json
+import os
+import pickle
+import numpy as np
+from collections import defaultdict
+from functools import partial
+from tqdm import tqdm
+from datetime import date
+
+from utils.activation_functions import quantized_relu_ste
+from utils.loss_functions import margin_loss
+from utils import ScRRAMBLe_routing, intercore_connectivity
+
+
+import tensorflow_datasets as tfds  # TFDS to download MNIST.
+import tensorflow as tf  # TensorFlow / `tf.data` operations.
+
+# -------------------------------------------------------------------
+# Auxiliary functions
+# ------------------------------------------------------------------
+def save_metrics(metrics_dict, filename):
+    """
+    Save the metrics to a file.
+    Args:
+        metrics_dict: dict, metrics to save.
+        filename: str, name of the file to save the metrics to.
+    """
+
+    metrics_dir = "/local_disk/vikrant/scrramble/logs"
+    filename = os.path.join(metrics_dir, filename)
+
+    with open(filename, 'wb') as f:
+        pickle.dump(metrics_dict, f)
+
+    print(f"Metrics saved to {filename}")
+
+    
+def save_model(state, filename):
+    """
+    Save the model state in a specified file
+    """
+
+    checkpoint_dir = "/local_disk/vikrant/scrramble/models"
+    filename_ = os.path.join(checkpoint_dir, filename)
+
+    with open(filename_, 'wb') as f:
+        pickle.dump(state, f)
+    
+    print(f"Model saved to {filename_}")
+
+# -------------------------------------------------------------------
+# Define the MNIST CapsNet Model
+# -------------------------------------------------------------------
+
+class ScRRAMBLeCapsNet(nnx.Module):
+    """
+    ScRRAMBLe CapsNet model for MNIST classification.
+    """
+
+    def __init__(
+            self,
+            input_vector_size: int, # size of flattened input vector
+            capsule_size: int, # size of each capsule e.g. 256 (number of columns/rows of a core)
+            receptive_field_size: int, # size of each receptive field e.g. 64 (number of columns/rows of a slot)
+            connection_probability: float, # fraction of total receptive fields on sender side that each receiving slot/receptive field takes input from
+            rngs: nnx.Rngs,
+            layer1_capsules: int = 20, # number of capsules in the first layer
+            layer2_capsules: int = 10, # number of capsules in the second layer
+    ):
+        
+        self.input_vector_size = input_vector_size
+        self.capsule_size = capsule_size
+        self.receptive_field_size = receptive_field_size
+        self.rngs = rngs
+        self.connection_probability = connection_probability
+        self.layer1_capsules = layer1_capsules
+        self.layer2_capsules = layer2_capsules
